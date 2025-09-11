173d897f813321b91877157bc70bb702b225d794
Matmil01 <matmil01@easv365.dk>
2025-09-11 14:43:07 +0200

    Firebased

--- src/views/UserUI/CartView.vue
+++ src/views/UserUI/CartView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="max-w-3xl mx-auto p-8 text-MyWhite">
+  <div class="max-w-3xl mx-auto p-8 text-MyWhite font-main">
     <h1 class="text-2xl font-bold mb-6">Your Cart</h1>
     <div v-if="cart.items.length">
       <div
